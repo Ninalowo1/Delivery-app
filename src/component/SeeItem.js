4b1d2ae855c0e02f61c52181b457fd10c87cc614
Ninalowo <wumidire@gmail.com>
2025-02-08 08:14:21 -0700

    Added more component

--- src/component/SeeItem.js
+++ src/component/SeeItem.js
@@ -56,35 +56,7 @@ const SeeItem=   () => {
     title="Veggie
      tomato mix"
     />
-        <SeeCard
-        imgUrl="https://wallpaperaccess.com/full/767054.jpg"
-    title="Drinks"
-    />
-        <SeeCard
-        imgUrl="https://th.bing.com/th/id/OIP.bejPmWStqSwDGK9HrrG-FwHaFz?rs=1&pid=ImgDetMain"
-    title="Snacks"
-    />
-        <SeeCard
-        imgUrl="https://th.bing.com/th/id/OIP.SWVTmBw5LjxWf12Oqgrd4AHaFj?rs=1&pid=ImgDetMain"
-    title="Sauce"
-    />
-      <SeeCard
-    imgUrl="https://wallpaperaccess.com/full/767054.jpg"
-    
-    title="Veggie tomato mix"
-    />
-        <SeeCard
-        imgUrl="https://wallpaperaccess.com/full/767054.jpg"
-    title="Drinks"
-    />
-        <SeeCard
-        imgUrl="https://th.bing.com/th/id/OIP.bejPmWStqSwDGK9HrrG-FwHaFz?rs=1&pid=ImgDetMain"
-    title="Snacks"
-    />
-        <SeeCard
-        imgUrl="https://th.bing.com/th/id/OIP.SWVTmBw5LjxWf12Oqgrd4AHaFj?rs=1&pid=ImgDetMain"
-    title="Sauce"
-    />
+       
 
 
 
